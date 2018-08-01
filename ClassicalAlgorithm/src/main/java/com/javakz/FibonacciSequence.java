6b5ea351750cc3ad94554dc5c2bdf62e74383bc4
yandongquan <yandongquanlove@gmail.com>
2018-08-01 10:01:32 +0800

    :mouse: update FibonacciSequence

--- ClassicalAlgorithm/src/main/java/com/javakz/FibonacciSequence.java
+++ ClassicalAlgorithm/src/main/java/com/javakz/FibonacciSequence.java
@@ -21,4 +21,26 @@ package com.javakz;
  */
 public class FibonacciSequence {
 
+    private static void achieve(int y) {
+        int i = 0;
+        for (i=1;i<=y;i++) {
+            System.out.print(f(i)+" ");
+        }
+    }
+
+    private static int f(int x) {
+        if (x==1 || x==2) {
+            return 1;
+        } else {
+            return f(x-1)+f(x-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--------------------start-------------------------");
+        achieve(20);
+        System.out.println();
+        System.out.println("--------------------end---------------------------");
+    }
+
 }
